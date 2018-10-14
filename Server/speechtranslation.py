78c2698b94c5ec30bd3d58e57f24d133be7eca53
Ryan Miller <ryanvmiller@icloud.com>
2018-10-13 21:39:01 -0500

    Removed google credentials

--- Server/speechtranslation.py
+++ Server/speechtranslation.py
@@ -126,7 +126,7 @@ def listen_print_loop(responses):
 
         else:
             print(transcript + overwrite_chars)
-            
+
 
             # Exit recognition if any of the transcribed phrases could be
             # one of our keywords.
